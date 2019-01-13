f61d00694f8b9916033f09f4e2a3b24ae8f68ec4
UltraTechX <ultratechx360@gmail.com>
2019-01-13 04:03:35 -0500

    added JQuery for sign in button detection, not complete yet

---
+++ js/NavBarAccountButton.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  if (!!$.cookie('authcheck')) {
+    // have cookie
+    var username = $.cookie('authcheck');
+    $.post('php/authCheck.php',
+       { authkey: username },
+       function(data){
+            //per the comment below,
+            //assuming data will simply
+            //be the username
+            var aconfirm = data.authConfirm; // John
+            var usrid = data.userID;
+            if(aconfirm == true){
+              $('#accInfoBox').replaceWith("<a href='' class='btn btn-primary btn-lg active' role='button' aria-pressed='true'>"+usrid+"</a>");
+            }else{
+              $('#accInfoBox').replaceWith("<a href='' class='btn btn-outline-secondary' role='button' aria-pressed='false'>Sign In</a>");
+            }
+       });
+  }else{
+    $('#accInfoBox').replaceWith("<a href='' class='btn btn-outline-secondary' role='button' aria-pressed='false'>Sign In</a>");
+  }
+});
