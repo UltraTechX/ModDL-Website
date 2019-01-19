e949c0aab46be88e6346463d354fbfec5b15ac20
UltraTechX <ultratechx360@gmail.com>
2019-01-18 23:47:59 -0500

    last time I touch this for now while I flesh out the account system

--- js/NavBarAccountButton.js
+++ js/NavBarAccountButton.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
             var aconfirm = data.authConfirm; // John
             var usrid = data.userID;
             if(aconfirm == true){
-              document.getElementById("accRegisterBox").replaceWith('<a href="" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">"+usrid+"</a>');
+              //document.getElementById("accRegisterBox").replaceWith('<a href="" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">"+usrid+"</a>');
             }else{
               $(document.getElementById("accInfoBox")).append($("<a href='https://opifexdev.net/user/signin/' class='btn btn-outline-secondary' role='button' aria-pressed='false'>Sign In</a>"));
               $(document.getElementById("accRegisterBox")).append($("<a href='https://opifexdev.net/user/register/' class='btn btn-outline-secondary' role='button' aria-pressed='false'>Register</a>"));
