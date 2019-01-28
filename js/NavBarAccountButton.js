3863fc1c6da8774096df314455ddbb4edd7cd6ce
UltraTechX <ultratechx360@gmail.com>
2019-01-27 21:57:08 -0500

    finished registration form, fixed some navbar links.

--- js/NavBarAccountButton.js
+++ js/NavBarAccountButton.js
@@ -13,12 +13,12 @@ $(document).ready(function(){
             if(aconfirm == true){
               //document.getElementById("accRegisterBox").replaceWith('<a href="" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">"+usrid+"</a>');
             }else{
-              $(document.getElementById("accInfoBox")).append($("<a href='https://opifexdev.net/user/signin' class='btn btn-outline-secondary' role='button' aria-pressed='false'>Sign In</a>"));
-              $(document.getElementById("accRegisterBox")).append($("<a href='https://opifexdev.net/user/register' class='btn btn-outline-secondary' role='button' aria-pressed='false'>Register</a>"));
+              $(document.getElementById("accInfoBox")).append($("<a href='https://opifexdev.net/user/signin/' class='btn btn-outline-secondary' role='button' aria-pressed='false'>Sign In</a>"));
+              $(document.getElementById("accRegisterBox")).append($("<a href='https://opifexdev.net/user/register/' class='btn btn-outline-secondary' role='button' aria-pressed='false'>Register</a>"));
             }
        });
   }else{
-    $(document.getElementById("accInfoBox")).append($("<a href='https://opifexdev.net/user/signin' class='btn btn-outline-secondary' role='button' aria-pressed='false'>Sign In</a>"));
-    $(document.getElementById("accRegisterBox")).append($("<a href='https://opifexdev.net/user/register' class='btn btn-outline-secondary' role='button' aria-pressed='false'>Register</a>"));
+    $(document.getElementById("accInfoBox")).append($("<a href='https://opifexdev.net/user/signin/' class='btn btn-outline-secondary' role='button' aria-pressed='false'>Sign In</a>"));
+    $(document.getElementById("accRegisterBox")).append($("<a href='https://opifexdev.net/user/register/' class='btn btn-outline-secondary' role='button' aria-pressed='false'>Register</a>"));
   }
 });
--- user/register/js/register.js
+++ user/register/js/register.js
@@ -11,14 +11,28 @@ $(document).ready(function(){
             var aconfirm = data.authConfirm; // John
             var usrid = data.userID;
             if(aconfirm == true){
-              window.location.replace("https://opifexdev.net/user/profile");
+              window.location.replace("https://opifexdev.net/user/profile/");
             }else{
-              $(document.getElementById("accInfoBox")).append($("<a href='https://opifexdev.net/user/signin' class='btn btn-outline-secondary' role='button' aria-pressed='false'>Sign In</a>"));
-              $(document.getElementById("accRegisterBox")).append($("<a href='https://opifexdev.net/user/register' class='btn btn-outline-secondary' role='button' aria-pressed='false'>Register</a>"));
+              $(document.getElementById("accInfoBox")).append($("<a href='https://opifexdev.net/user/signin/' class='btn btn-outline-secondary' role='button' aria-pressed='false'>Sign In</a>"));
+              $(document.getElementById("accRegisterBox")).append($("<a href='https://opifexdev.net/user/register/' class='btn btn-outline-secondary' role='button' aria-pressed='false'>Register</a>"));
             }
        });
   }else{
-    $(document.getElementById("accInfoBox")).append($("<a href='https://opifexdev.net/user/signin' class='btn btn-outline-secondary' role='button' aria-pressed='false'>Sign In</a>"));
-    $(document.getElementById("accRegisterBox")).append($("<a href='https://opifexdev.net/user/register' class='btn btn-outline-secondary' role='button' aria-pressed='false'>Register</a>"));
+    $(document.getElementById("accInfoBox")).append($("<a href='https://opifexdev.net/user/signin/' class='btn btn-outline-secondary' role='button' aria-pressed='false'>Sign In</a>"));
+    $(document.getElementById("accRegisterBox")).append($("<a href='https://opifexdev.net/user/register/' class='btn btn-outline-secondary' role='button' aria-pressed='false'>Register</a>"));
   }
+});
+
+var b1 = document.getElementById("button_register");
+
+$('#registerForm').submit(function(){
+    $.ajax({
+      url: $('#registerForm').attr('action'),
+      type: 'POST',
+      data : $('#registerForm').serialize(),
+      success: function(response){
+        console.log('return code '+response);
+      }
+    });
+    return false;
 });
