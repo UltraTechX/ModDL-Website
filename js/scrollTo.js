20f8698d7d08aa513538bb612928b4b8f134f583
UltraTechX <ultratechx360@gmail.com>
2019-03-20 21:45:30 -0400

    all thats left for the current site is finishing the homepage, will probably wait until website is close to completion to finishing it.

--- js/scrollTo.js
+++ js/scrollTo.js
@@ -1,21 +1,6 @@
-function scrollTo(to, duration) {
-    if (document.body.scrollTop == to) return;
-    var diff = to - document.body.scrollTop;
-    var scrollStep = Math.PI / (duration / 10);
-    var count = 0, currPos;
-    start = element.scrollTop;
-    scrollInterval = setInterval(function(){
-        if (document.body.scrollTop != to) {
-            count = count + 1;
-            currPos = start + diff * (0.5 - 0.5 * Math.cos(count * scrollStep));
-            document.body.scrollTop = currPos;
-        }
-        else { clearInterval(scrollInterval); }
-    },10);
-}
-
-function test(elID)
-{
-    var dest = document.getElementById(elID);
-    scrollTo(dest.offsetTop, 500);
-}
+$(document).ready(function(){
+    $( "a.scrollLink" ).click(function( event ) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top-50 }, 500);
+    });
+});
